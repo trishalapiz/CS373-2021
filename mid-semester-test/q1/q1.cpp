c94620d609be3b31ae2db5c61dc095e80fe1ae0d
Trisha Lapiz <52408498+trishalapiz@users.noreply.github.com>
2021-04-08 22:29:51 +1200

    Update q1.cpp
    take out the code that didn't work

--- mid-semester-test/q1/q1.cpp
+++ mid-semester-test/q1/q1.cpp
@@ -46,110 +46,6 @@ printf("u = {%.2lf, %.2lf, %.2lf}", u.x, u.y, u.z);
 
 
 */
-#include<stdio.h>
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-class Vector3	//The vector3 class is used in several Coderunner questions, in these questions it is already defined for you.
-{
-	public:
-	double x;
-	double y;
-	double z;
-	Vector3()	//default constructor
-	{
-		x = 0.0;
-		y = 0.0;
-		z = 0.0;
-	}
-	Vector3(double xVal, double yVal, double zVal)	//non-default constructor
-	{
-		x = xVal;
-		y = yVal;
-		z = zVal;
-	}
-
-    double dot(Vector3 u, Vector3 v) {
-		return ( (u.x * v.x) + (u.y * v.y) + (u.z * v.z) ); 
-
-	}
-
-    Vector3 cross(Vector3 a, Vector3 b) {
-        return Vector3((a.y * b.z) - (a.z * b.y), ((a.z * b.x) - (a.x * b.z)), ((a.x * b.y) - (a.y * b.x)));
-    }
-
-    // double magnitude(Vector3 v) {
-    //     return sqrt((v.x * v.x) + (v.y * v.y) + (v.z * v.z));
-    // }
-
-    
-    
-};
-
-
-    
-    
-Vector3 getOriginalVector(Vector3 uNormalized, Vector3 uv) {
-    // angle between 2 vectors
-    // Vector3 cp = cross(uNormalized, uv);
-
-    // double uvMag = uv.magnitude();
-    // double uNMag = uNormalized.magnitude(); // should return 1
-
-    // double uMag = uvMag + uNMag;
-
-    // double angleBetweenUVAndUN = dot(uNormalized, uv) / (uNMag * uvMag); // this is cos Θ
-    // double uvUNAngle = acos(angleBetweenUVAndUN) * (180/M_PI); // returns 0.64 rads if not converted
-
-    // // return Vector3(uMag * uNormalized.x, uMag * uNormalized.y, uMag * uNormalized.z);
-
-    // // scale both vectors so that they form a right triangle
-    // Vector3 scaledUN = Vector3(uvMag * uNormalized.x, uvMag * uNormalized.y, uvMag * uNormalized.z); // scale uNormalized so that it's the same length as Uv
-    // Vector3 scaledUV = Vector3(angleBetweenUVAndUN * uv.x, angleBetweenUVAndUN * uv.y, angleBetweenUVAndUN * uv.z);
-
-    // Vector3 scaledUNToScaledUV = 
-
-
-
-    // double findAngle = (dot(scaledUN, uv) / (scaledUN.magnitude() * uvMag));
-    
-    // double angle = acos(findAngle); // returns the angle between uNormalised and Uv
-    
-    // return cp;
-}
-
-// int main() {
-//     Vector3 un = Vector3(0.8, 0.6, 0.0);                 
-//     Vector3 uv = Vector3(4.0, 0.0, 0.0);
-//     Vector3 h = Vector3();
-//     Vector3 u = h.getOriginalVector(un, uv);
-
-//     cout << u;
-
-//     return 0;
-// }
-
-// main();
-
-
-// double uvMag = uv.magnitude();
-//     double uNMag = uNormalized.magnitude(); // should return 1
-
-//     Vector3 scaledUN = Vector3(uvMag * uNormalized.x, uvMag * uNormalized.y, uvMag * uNormalized.z); // scale uNormalized so that it's the same length as Uv
-//     Vector3 resultantOne = scaledUN - uv;
-//     double resOneMag = resultantOne.magnitude(); // 2.529822128
-
-
-//     // double angleBetweenUVAndUN = dot(uNormalized, uv) / (uNMag * uvMag); // this is cos Θ
-//     // double uvUNAngle = acos(angleBetweenUVAndUN) * (180/M_PI); // returns 0.64 rads if not converted
-
-//     double cosTheta = dot(resultantOne, scaledUN) / (resOneMag * uvMag);
-//     double x = acos(cosTheta) * (180/M_PI);
-
-
-
-
 // FINAL CODE
 
 Vector3 getOriginalVector(Vector3 uNormalized, Vector3 uv) {
@@ -171,8 +67,4 @@ Vector3 getOriginalVector(Vector3 uNormalized, Vector3 uv) {
     double scalar = uvMag / dp;
 
     return Vector3(scalar * uNormalized.x, scalar * uNormalized.y, scalar * uNormalized.z);
-
-
-
-
 }
